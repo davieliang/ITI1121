cd5f57e0dba674048057774fef24f179e2a9345c
Matthew Langlois <fletchto99@gmail.com>
2015-01-26 12:12:22 -0500

    Part 1 & 2 of Lab 2

---
+++ Labs/Lab 2/TestAll.java
@@ -0,0 +1,57 @@
+/* ITI 1121. Introduction to Computing II; Laboratory 2
+ * ITI 1521. Introduction a l'informatique II; Laboratoire 2
+ */
+
+/** Runs all the tests using a textual runner.  This is the
+ *  alternative to running all the tests within your favorite
+ *  development environment (DrJava, Eclipse...).  This may require
+ *  downloading JUnit from 
+ *  <a href="https://github.com/KentBeck/junit/wiki/Download-and-Install">github.com/KentBeck/junit/wiki/Download-and-Install</a>.
+ *
+ * <pre>
+ * > javac -cp junit.jar:. RunTests.java
+ * > java -cp junit.jar:. RunTests
+ * ************************************************************
+ * *                                                          *
+ * *                                                          *
+ * *                                                          *
+ * *                                                          *
+ * ************************************************************
+ * 
+ * ..............
+ * Time: 0.031
+ * 
+ * OK (14 tests)
+ *  * </pre>
+ * @author Marcel Turcotte (turcotte@site.uottawa.ca)
+ */
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class TestAll {
+  
+    // Creates and returns a TestSuite object.
+
+    private static Test suite() {
+
+        TestSuite suite = new TestSuite();
+  
+        suite.addTestSuite( TestFindAndReplace.class );
+
+        return suite;
+    }
+
+    /**
+     * Runs the test suite using the textual runner.
+     *
+     * @param args reference to an array of arguments passed to the program on the command line
+     */
+    
+    public static void main( String[] args ) {
+
+        StudentInfo.display();
+
+        junit.textui.TestRunner.run( suite() );
+    }
+}
