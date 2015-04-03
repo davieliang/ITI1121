32380f7fa657d119b5dda325dd0e2537494654d4
Matthew Langlois <fletchto99@gmail.com>
2015-04-03 16:42:40 -0400

    Assignment 4 Part 4

--- Assignments/Assignment 4/us/mattandjoe/assignment4/part4/Frequency.java
+++ Assignments/Assignment 4/us/mattandjoe/assignment4/part4/Frequency.java
@@ -4,11 +4,29 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Frequency {
-	public static void frequency(List<Tuple> l) {
-		String res;
-		Iterator<Tuple> i = l.iterator();
-		while (i.hasNext()) {
-			Iterator<Tuple> j = l.iterator();	
-		}
-	}
+    public static void frequency(List<Tuple> l) {
+        String result = "";
+        Iterator<Tuple> i = l.iterator();
+        while (i.hasNext()) {
+            Tuple eval = i.next();
+            if (!eval.visited()) {
+                char current = eval.getChar();
+                int count = 1;
+                eval.toggle();
+                Iterator<Tuple> j = l.iterator();
+
+                while (j.hasNext()) {
+                    Tuple next = j.next();
+                    if (!next.visited()) {
+                        if (next.getChar() == current) {
+                            count++;
+                            next.toggle();
+                        }
+                    }
+                }
+                result += current + " : " + count + ", ";
+            }
+        }
+        System.out.println(result.substring(0, result.length() - 2));
+    }
 }
